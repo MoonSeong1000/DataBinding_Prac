f69da6c188bf72bb567fe2194526e975e739b399
munseongcheon <munseong.cheon@plustv.io>
2020-05-13 22:25:32 +0900

    dataBinding 기본

--- app/src/main/java/com/example/databinding_prac/MainActivity.kt
+++ app/src/main/java/com/example/databinding_prac/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.databinding_prac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding_prac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        var binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.tvSample.text = "Hello~~"
     }
 }
---
+++ app/src/main/java/com/example/databinding_prac/User.kt
@@ -0,0 +1,7 @@
+package com.example.databinding_prac
+
+data class User(
+    var name: String = ""
+    , var address: String = ""
+    , var profileURL: Int = -1
+)
