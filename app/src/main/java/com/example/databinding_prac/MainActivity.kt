aaa279bea48c74730b3fd86e41f3d5bd74a638b7
munseongcheon <munseong.cheon@plustv.io>
2020-05-15 23:08:42 +0900

    erase code

--- app/src/main/java/com/example/databinding_prac/MainActivity.kt
+++ app/src/main/java/com/example/databinding_prac/MainActivity.kt
@@ -7,25 +7,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.databinding_prac.databinding.ActivityMainBinding
-import com.example.databinding_prac.factory.MainViewModelFactory
-import com.example.databinding_prac.viewmodel.MainViewModel
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        var binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        var factory = MainViewModelFactory()
-        var viewModel: MainViewModel = ViewModelProvider(this,factory).get(MainViewModel::class.java)
-
-        viewModel.clickConverter.observe(this, Observer {
-            Toast.makeText(this, "${binding.user?.name}, ${binding.user?.address}", Toast.LENGTH_SHORT).show()
-        })
-        binding.user = User("천문성","안산시",-1)
-        binding.viewModel = viewModel
-        binding.setLifecycleOwner(this)
     }
 }
--- app/src/main/java/com/example/databinding_prac/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.databinding_prac
-
-data class User(
-    var name: String = ""
-    , var address: String = ""
-    , var profileURL: Int = -1
-)
